8a93d79bb93076c2bcd77e347e6c7bdc03f70e3c
leeys9423 <leeys9423@gmail.com>
2025-01-23 16:02:42 +0900

    refactor: Notice 엔티티 마이그레이션

--- src/main/java/com/redbox/domain/notice/entity/Notice.kt
+++ src/main/java/com/redbox/domain/notice/entity/Notice.kt
@@ -1,71 +1,52 @@
-package com.redbox.domain.notice.entity;
+package com.redbox.domain.notice.entity
 
-import com.redbox.domain.attach.entity.AttachFile;
-import com.redbox.domain.attach.exception.NullAttachFileException;
-import com.redbox.domain.notice.dto.UpdateNoticeRequest;
-import com.redbox.domain.user.entity.User;
-import com.redbox.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.redbox.domain.attach.entity.AttachFile
+import com.redbox.domain.notice.dto.UpdateNoticeRequest
+import com.redbox.domain.user.entity.User
+import com.redbox.global.entity.BaseEntity
+import jakarta.persistence.*
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Getter
 @Entity
 @Table(name = "notices")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Notice extends BaseEntity {
-
+class Notice(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "notice_id")
-    private Long id;
+    var id: Long? = null,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private User user;
-    private String noticeTitle;
+    var user: User,
+
+    var noticeTitle: String,
 
     @Lob
-    private String noticeContent;
+    var noticeContent: String,
 
-    private int noticeHits;
+    var noticeHits: Int
+) : BaseEntity() {
 
-    @OneToMany(mappedBy = "notice", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<AttachFile> attachFiles = new ArrayList<>();
-
-    @Builder
-    public Notice(User user, String noticeTitle, String noticeContent, int noticeHits) {
-        this.user = user;
-        this.noticeTitle = noticeTitle;
-        this.noticeContent = noticeContent;
-        this.noticeHits = noticeHits;
-    }
+    @OneToMany(mappedBy = "notice", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var attachFiles: MutableList<AttachFile> = mutableListOf()
 
     // 연관관계 편의 메서드
-    // 비즈니스 로직 상 글 기준으로 움직이기 때문에 여기에 선언
-    public void addAttachFiles(AttachFile attachFile) {
-        attachFile.validateNull();
-        if (attachFile.isDuplicateIn(this.attachFiles)) return;
+    fun addAttachFiles(attachFile: AttachFile) {
+        attachFile.validateNull()
+        if (attachFile.isDuplicateIn(this.attachFiles)) return
 
-        this.attachFiles.add(attachFile);
-        attachFile.setNotice(this);
+        this.attachFiles.add(attachFile)
+        attachFile.notice = this
     }
 
-    public void removeAttachFiles(AttachFile attachFile) {
-        attachFile.validateNull();
+    fun removeAttachFiles(attachFile: AttachFile) {
+        attachFile.validateNull()
 
-        this.attachFiles.remove(attachFile);
-        attachFile.setNotice(null);
+        this.attachFiles.remove(attachFile)
+        attachFile.notice = null
     }
 
-    public void updateNotice(UpdateNoticeRequest request) {
-        this.noticeTitle = request.getTitle();
-        this.noticeContent = request.getContent();
+    fun updateNotice(request: UpdateNoticeRequest) {
+        this.noticeTitle = request.title
+        this.noticeContent = request.content
     }
-
 }
