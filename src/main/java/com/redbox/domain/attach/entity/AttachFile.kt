7e937bc5e5985245548dddcd60292bffa5fb52f6
you <20211939@edu.hanbat.ac.kr>
2025-01-24 01:09:30 +0900

    refactor: request에서 funding 이름 변경

--- src/main/java/com/redbox/domain/attach/entity/AttachFile.kt
+++ src/main/java/com/redbox/domain/attach/entity/AttachFile.kt
@@ -1,84 +1,57 @@
-package com.redbox.domain.attach.entity;
+package com.redbox.domain.attach.entity
 
-import com.redbox.domain.attach.exception.NullAttachFileException;
-import com.redbox.domain.funding.entity.Funding;
-import com.redbox.domain.notice.entity.Notice;
-import com.redbox.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.redbox.domain.attach.exception.NullAttachFileException
+import com.redbox.domain.notice.entity.Notice
+import com.redbox.domain.funding.entity.Funding
+import com.redbox.global.entity.BaseEntity
+import jakarta.persistence.*
 
-import java.util.List;
-
-@Getter
 @Entity
 @Table(name = "attach_files")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class AttachFile extends BaseEntity {
-
+class AttachFile (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "attach_id")
-    private Long id;
+    var id: Long? = null,
 
     @Enumerated(EnumType.STRING)
-    private Category category;
+    var category: Category,
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "request_id")
-    private Funding funding;
+    @JoinColumn(name = "funding_id")
+    var funding: Funding? = null,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "notice_id")
-    private Notice notice;
+    var notice: Notice? = null,
 
-    private String originalFilename;
-    private String newFilename;
+    var originalFilename: String,
+    var newFilename: String
+) : BaseEntity() {
 
-    @Builder
-    public AttachFile(Category category, Notice notice, Funding funding, String originalFilename, String newFilename) {
-        this.category = category;
-        this.notice = notice;
-        this.funding = funding;
-        this.originalFilename = originalFilename;
-        this.newFilename = newFilename;
-    }
-
-    // 연관 관계 편의 메서드를 위한 setter 메서드
-    @SuppressWarnings("lombok")
-    public void setNotice(Notice notice) {
-        this.notice = notice;
-    }
-    @SuppressWarnings("lombok")
-    public void setFunding(Funding funding) {
-        this.funding = funding;
-    }
-
-    public void validateNull() {
+    fun validateNull() {
         if (this == null) {
-            throw new NullAttachFileException();
+            throw NullAttachFileException()
         }
     }
 
-    public boolean isDuplicateIn(List<AttachFile> attachFiles) {
-        return attachFiles.contains(this);
+    fun isDuplicateIn(attachFiles: List<AttachFile?>): Boolean {
+        return attachFiles.contains(this)
     }
 
-    public boolean belongToPost(Long postId) {
-        return switch (this.category) {
-            case NOTICE -> isNoticeFile(postId);
-            case FUNDING -> isRequestFile(postId);
-        };
+    fun belongToPost(postId: Long): Boolean {
+        return when (this.category) {
+            Category.NOTICE -> isNoticeFile(postId)
+            Category.FUNDING -> isFundingFile(postId)
+        }
     }
 
-    private boolean isNoticeFile(Long postId) {
+    private fun isNoticeFile(postId: Long): Boolean {
         // notice_id 값이 있다면, notice 필드를 프록시 객체로 설정하기 때문에
-        return this.notice != null;
+        return this.notice != null
     }
 
-    private boolean isRequestFile(Long postId) {
-        return this.funding != null;
+    private fun isFundingFile(postId: Long): Boolean {
+        return this.funding != null
     }
 }
