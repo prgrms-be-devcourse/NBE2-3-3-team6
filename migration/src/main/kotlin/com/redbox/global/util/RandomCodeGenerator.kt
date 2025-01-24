61bdb7bf1c2b60c19475d8f73ab91546ce66fbbf
leeys9423 <leeys9423@gmail.com>
2025-01-24 12:11:19 +0900

    refactor: util 패키지 마이그레이션 - static final 상수들 body 영역 안으로 이동

--- migration/src/main/kotlin/com/redbox/global/util/RandomCodeGenerator.kt
+++ migration/src/main/kotlin/com/redbox/global/util/RandomCodeGenerator.kt
@@ -2,15 +2,14 @@ package com.redbox.global.util
 
 import java.security.SecureRandom
 
-private const val CHARACTERS: String = "abcdefghijklmnopqrstuvwxyz0123456789"
-private val SECURE_RANDOM: SecureRandom = SecureRandom()
-private const val CODE_LENGTH: Int = 8
-
 class RandomCodeGenerator {
-
     companion object {
+        private const val CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789"
+        private val SECURE_RANDOM = SecureRandom()
+        private const val CODE_LENGTH = 8
+
         fun generateRandomCode(): String {
-            val randomCode: StringBuilder = StringBuilder()
+            val randomCode = StringBuilder()
 
             for (i in 0 until CODE_LENGTH) {
                 val index = SECURE_RANDOM.nextInt(CHARACTERS.length)
