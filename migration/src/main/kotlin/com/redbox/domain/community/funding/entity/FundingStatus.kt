805fd63f8b82e261ee513184ac5c99992f070de0
you <20211939@edu.hanbat.ac.kr>
2025-01-24 14:27:10 +0900

    refactor: Funding enum 클래스 마이그레이션

---
+++ migration/src/main/kotlin/com/redbox/domain/community/funding/entity/FundingStatus.kt
@@ -0,0 +1,5 @@
+package com.redbox.domain.community.funding.entity
+
+enum class FundingStatus(val text: String? = null) {
+    REQUEST("요청"), APPROVE("승인"), REJECT("거절"), EXPIRED("만료"), IN_PROGRESS("진행중"), DROP("삭제");
+}
---
+++ migration/src/main/kotlin/com/redbox/domain/community/funding/entity/Priority.kt
@@ -0,0 +1,5 @@
+package com.redbox.domain.community.funding.entity
+
+enum class Priority(val text: String? = null) {
+    HIGH("상"), MEDIUM("중"), LOW("하");
+}
