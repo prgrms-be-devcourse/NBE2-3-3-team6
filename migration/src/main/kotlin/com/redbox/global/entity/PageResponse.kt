19b300e90fd9d2a6f7e18f6cf19b3707989c68cd
Jhynguu <jhyngu0531@gmail.com>
2025-01-24 11:31:34 +0900

    refactor: 글로벌 Entity 클래스 코틀린 마이그레이션
    - BaseEntity
    - BaseTimeEntity
    - PageResponse

---
+++ migration/src/main/kotlin/com/redbox/global/entity/PageResponse.kt
@@ -0,0 +1,21 @@
+package com.redbox.global.entity
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import org.springframework.data.domain.Page
+
+@JsonDeserialize
+data class PageResponse<T>(
+    val content: List<T> = emptyList(),
+    val currentPage: Int = 0,
+    val size: Int = 0,
+    val totalElements: Long = 0,
+    val totalPages: Int = 0
+) {
+    constructor(page: Page<T>) : this(
+        content = page.content,
+        currentPage = page.number + 1, // 0부터 시작하므로 1을 더해줌
+        size = page.size,
+        totalElements = page.totalElements,
+        totalPages = page.totalPages
+    )
+}
