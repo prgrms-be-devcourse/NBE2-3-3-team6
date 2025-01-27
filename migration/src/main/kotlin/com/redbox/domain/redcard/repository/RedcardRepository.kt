8fb4ba47960fb277bc731342f3e9e452202cbf8e
Jhynguu <jhyngu0531@gmail.com>
2025-01-27 10:36:22 +0900

    refactor: RedcardRepository 헌혈증 조회 메서드 코틀린으로 마이그레이션
    - 사용자 ID로 헌혈증 목록을 조회할 수 있는 메서드 변환

--- migration/src/main/kotlin/com/redbox/domain/redcard/repository/RedcardRepository.kt
+++ migration/src/main/kotlin/com/redbox/domain/redcard/repository/RedcardRepository.kt
@@ -1,9 +1,15 @@
 package com.redbox.domain.redcard.repository
 
 import com.redbox.domain.redcard.entity.Redcard
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.Optional
 
 interface RedcardRepository : JpaRepository<Redcard, Long> {
+
     fun findBySerialNumber(serialNumber: String): Optional<Redcard>
+
+    fun findAllByUserId(userId: Long, pageable: Pageable): Page<Redcard>
+
 }
