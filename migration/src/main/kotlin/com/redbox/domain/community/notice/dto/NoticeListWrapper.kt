a52cfcc4a8562920e81de7d3f44869fbefe23745
you <20211939@edu.hanbat.ac.kr>
2025-02-02 22:02:05 +0900

    refactor: @JsontypeInfo 추가 - 객체 타입 JSON 데이터에 포함시키기 위함

--- migration/src/main/kotlin/com/redbox/domain/community/notice/dto/NoticeListWrapper.kt
+++ migration/src/main/kotlin/com/redbox/domain/community/notice/dto/NoticeListWrapper.kt
@@ -1,9 +1,11 @@
 package com.redbox.domain.community.notice.dto
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 
 @JsonDeserialize
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 data class NoticeListWrapper(
     val notices: List<RecentNoticeResponse>
 ) {
