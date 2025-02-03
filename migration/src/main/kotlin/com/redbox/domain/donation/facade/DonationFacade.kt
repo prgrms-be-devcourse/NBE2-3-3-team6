371ad4d33950c2157c0a356827a03af1509ccd27
Bin-Choi <csb5268@gmail.com>
2025-02-04 01:57:54 +0900

    refactor: 게시글 자동 만료 처리 & 연관 기부 완료 처리

--- migration/src/main/kotlin/com/redbox/domain/donation/facade/DonationFacade.kt
+++ migration/src/main/kotlin/com/redbox/domain/donation/facade/DonationFacade.kt
@@ -3,7 +3,9 @@ package com.redbox.domain.donation.facade
 import com.redbox.domain.donation.application.DonationService
 import com.redbox.domain.donation.dto.DonationListResponse
 import com.redbox.domain.donation.dto.ReceptionListResponse
+import com.redbox.domain.redcard.entity.Redcard
 import com.redbox.global.entity.PageResponse
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 
 @Component
@@ -24,5 +26,9 @@ class DonationFacade(
 
     fun getSumDonationAmountInRedbox(): Int? {
         return donationService.getSumDonationAmountInRedbox()
+    }
+
+    fun donationConfirm(fundingId: Long, receiverId: Long) {
+        return donationService.donationConfirm(fundingId, receiverId)
     }
 }
