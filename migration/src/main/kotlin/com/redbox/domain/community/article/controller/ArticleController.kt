d0259d29b505d1b4c6ecc38f28410fb56272764f
Bin-Choi <csb5268@gmail.com>
2025-02-04 04:46:43 +0900

    refactor: articleController migration

--- migration/src/main/kotlin/com/redbox/domain/community/article/controller/ArticleController.kt
+++ migration/src/main/kotlin/com/redbox/domain/community/article/controller/ArticleController.kt
@@ -21,39 +21,39 @@ class ArticleController(
     private val articleService: ArticleService
 ) {
 
-    @GetMapping("/articles")
-    fun getArticles(
-        @RequestParam(defaultValue = "1") page: Int,
-        @RequestParam(defaultValue = "10") size: Int
-    ): ResponseEntity<PageResponse<ArticleResponse>> {
-        return ResponseEntity.ok(articleService.getArticles(page, size))
-    }
-
-    @PostMapping("/articles")
-    fun createArticle(
-        @RequestBody request: @Valid CreateArticleRequest
-    ): ResponseEntity<Void> {
-        articleService.createArticle(request)
-        return ResponseEntity.status(HttpStatus.CREATED).build()
-    }
-
-    @PutMapping("/articles/{articleId}")
-    fun updateArticle(
-        @RequestBody @Valid request: CreateArticleRequest
-    ): ResponseEntity<Void> {
-        articleService.updateArticle(articleId, request);
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/articles/{articleId}")
-    fun getArticle(@PathVariable articleId: Long): ResponseEntity<ArticleResponse> {
-        return ResponseEntity.ok(articleService.getArticle(articleId));
-    }
-
-    @DeleteMapping("/articles/{articleId}")
-    fun deleteArticle(@PathVariable articleId : Long) : ResponseEntity<Void>
-    {
-        articleService.deleteArticle(articleId);
-        return ResponseEntity.ok().build();
-    }
+//    @GetMapping("/articles")
+//    fun getArticles(
+//        @RequestParam(defaultValue = "1") page: Int,
+//        @RequestParam(defaultValue = "10") size: Int
+//    ): ResponseEntity<PageResponse<ArticleResponse>> {
+//        return ResponseEntity.ok(articleService.getArticles(page, size))
+//    }
+//
+//    @PostMapping("/articles")
+//    fun createArticle(
+//        @RequestBody request: @Valid CreateArticleRequest
+//    ): ResponseEntity<Void> {
+//        articleService.createArticle(request)
+//        return ResponseEntity.status(HttpStatus.CREATED).build()
+//    }
+//
+//    @PutMapping("/articles/{articleId}")
+//    fun updateArticle(
+//        @RequestBody @Valid request: CreateArticleRequest
+//    ): ResponseEntity<Void> {
+//        articleService.updateArticle(articleId, request);
+//        return ResponseEntity.ok().build();
+//    }
+//
+//    @GetMapping("/articles/{articleId}")
+//    fun getArticle(@PathVariable articleId: Long): ResponseEntity<ArticleResponse> {
+//        return ResponseEntity.ok(articleService.getArticle(articleId));
+//    }
+//
+//    @DeleteMapping("/articles/{articleId}")
+//    fun deleteArticle(@PathVariable articleId : Long) : ResponseEntity<Void>
+//    {
+//        articleService.deleteArticle(articleId);
+//        return ResponseEntity.ok().build();
+//    }
 }
